6c893ccfb800793ca1551fe2c5e4c7bbff0599f4
Michael Moore <stuporglue@gmail.com>
2014-03-15 21:30:15 -0500

    Fishnet for production use

--- steps/Env_settings.ps1
+++ steps/Env_settings.ps1
@@ -1,5 +1,5 @@
-$env:PATH += ";D:\Scripts\solar_scripts-master\lastools\bin"
-$env:PATH += ";D:\Scripts\solar_scripts-master\steps"
+$env:PATH += ";D:\Scripts\solar_scripts\lastools\bin"
+$env:PATH += ";D:\Scripts\solar_scripts\steps"
 $env:PATH += ";C:\Python27\ArcGIS10.2"
 $env:PATH += ";C:\Users\moor1090\AppData\Local\Apps\2.0\2JH41ZG7.GQ2\YDNY5LK7.XN1\gith..tion_317444273a93ac29_0001.0002_6db6521c0dc514d4"
 $env:PATH += ";C:\Users\moor1090\AppData\Local\GitHub\PortableGit_054f2e797ebafd44a30203088cd3d58663c627ef\bin"
