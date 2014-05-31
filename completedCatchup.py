7c064d797e9e22f1fa2e389942f3933137d4bc12
Michael Moore <stuporglue@gmail.com>
2014-05-31 09:42:18 -0500

    CompletedCatchup by subdirs

--- completedCatchup.py
+++ completedCatchup.py
@@ -6,20 +6,15 @@ from config import *
 
 out_path = config.get('paths','solar_raster_output_dir')
 
-updatewhere = []
+print "Checking files in " + out_path
 
-max = 5000
+for dirname in glob.glob(out_path + "\\*"):
+    print "Processing " + dirname
+    updatewhere = []
+    for img in glob.glob(dirname + '\\*.img'):
+        updatewhere.append(img.replace(dirname + '\\SRR_','').replace('.img',''))
 
-for img in glob.glob(out_path + '\\*.img'):
-    updatewhere.append(img.replace(out_path + '\\SRR_','').replace('.img',''))
-
-    if len(updatewhere) > max:
-        print "Updating " + str(max)
+    if len(updatewhere) > 0:
+        print "Updating " + str(len(updatewhere))
         q = "UPDATE sa_fishnets SET state=2 WHERE state<>2 AND id IN (" + ','.join(updatewhere) + ")"
         dbconn.run_query(q)
-        updatewhere = []
-
-if len(updatewhere) > 0:
-    print "Updating " + str(len(updatewhere))
-    q = "UPDATE sa_fishnets SET state=2 WHERE state<>2 AND id IN (" + ','.join(updatewhere) + ")"
-    dbconn.run_query(q)
